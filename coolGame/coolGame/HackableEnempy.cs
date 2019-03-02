94770af2f42e4bffed8c22952f886af0a8684673
Anna Murphy <blm1003@rit.edu>
2019-03-02 17:11:55 -0500

    added super constructor

--- coolGame/coolGame/HackableEnempy.cs
+++ coolGame/coolGame/HackableEnempy.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace coolGame
 {
     class HackableEnempy : Enemy
     {
+
+        public HackableEnempy(Texture2D texture, Rectangle position, KeyboardState kbState)
+        : base(texture, position, kbState) { }
+
+        private bool hasBeenHacked;
+
         public override void Interact()
         {
             
