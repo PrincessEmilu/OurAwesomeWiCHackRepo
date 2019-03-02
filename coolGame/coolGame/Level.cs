2bcdbd0d6e4f32396f6f549feda20ee5d9082457
Anna Murphy <blm1003@rit.edu>
2019-03-02 14:58:45 -0500

    Method stubs outlined

--- coolGame/coolGame/Level.cs
+++ coolGame/coolGame/Level.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace coolGame
 {
-    public class Level
+    public class Level : GameObject
     {
         public Level()
         {
+        }
+
+        public override Update(GameTime gameTime)
+        {
+            return null;
+        }
+
+        public override Draw (SpriteBatch spritebatch)
+        {
+            return null;
         }
     }
 }
