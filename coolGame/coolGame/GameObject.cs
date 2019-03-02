a86e3d4285930b8e07a67d41c455aff90efaa35f
Emily Horton <emilyhorton1994@gmail.com>
2019-03-02 14:51:08 -0500

    Added parent GameObject

---
+++ coolGame/coolGame/GameObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace coolGame
+{
+    abstract class GameObject
+    {
+        //Must implement abstract and draw
+        abstract protected void Update(GameTime gameTime);
+        abstract protected void Draw(SpriteBatch spriteBatch);
+    }
+}
