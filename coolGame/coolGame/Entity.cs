a1a4e22c2b46c794db6705440fb5066efff979d5
Anna Murphy <blm1003@rit.edu>
2019-03-02 17:15:54 -0500

    super const

--- coolGame/coolGame/Entity.cs
+++ coolGame/coolGame/Entity.cs
@@ -11,11 +11,11 @@ namespace coolGame
 {
     abstract class Entity : GameObject
     {
-        public Entity (Texture2D texture, Rectangle position, KeyboardState kbState)
-        : base(texture, position, kbState) { }
-        
         //All entities need a texture and a rectangle
         protected Texture2D texture;
         protected Rectangle position;
+
+        public Entity (Texture2D texture, Rectangle position, KeyboardState kbState)
+        : base(texture, position, kbState) { }   
     }
 }
