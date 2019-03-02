29c759ecfc8416c4a40a8feb23c22fe13b504a03
Anna Murphy <blm1003@rit.edu>
2019-03-02 16:50:18 -0500

    added method stub

--- coolGame/coolGame/HackableEnempy.cs
+++ coolGame/coolGame/HackableEnempy.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace coolGame
 {
-    class Class1
+    class HackableEnempy : Enemy
     {
+        public override void Interact()
+        {
+            
+        }
     }
 }
