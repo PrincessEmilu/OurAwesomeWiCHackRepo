e5201f05e7941cc6f762049bef1910d5af8ccce9
Anna Murphy <blm1003@rit.edu>
2019-03-02 16:11:38 -0500

    Class for static methods started.:

---
+++ coolGame/coolGame/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace coolGame
+{
+    static class Helpers
+    {
+        public static bool CheckSingleKeyPress (Keys keys, KeyboardState kbState, KeyboardState pbState)
+        {
+            return kbState.IsKeyDown(keys) && pbState.IsKeyDown(keys;
+        }
+
+        // TODO: Further Static methods.
+
+    }
+}
