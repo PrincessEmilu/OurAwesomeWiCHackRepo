274bbb0b41cd706d70c9c6a7bbddd65c186e81e0
Anna Murphy <blm1003@rit.edu>
2019-03-02 23:08:47 -0500

    finished patroller

--- coolGame/coolGame/PatrolingGuard.cs
+++ coolGame/coolGame/PatrolingGuard.cs
@@ -10,13 +10,15 @@ namespace coolGame
 {
     class PatrolingGuard : HackableEnemy
     {
-        public PatrolingGuard(Texture2D texture, Rectangle position)
-            : base(texture, position) { }
 
         // Movement
         const int moveSpeed = 5;
         const int patrolLength = 200;
+        int amountMoved;
         bool patrolDirection = true;
+
+        public PatrolingGuard(Texture2D texture, Rectangle position)
+            : base(texture, position) { }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
@@ -28,10 +30,17 @@ namespace coolGame
             if (patrolDirection)
             {
                 position.X += moveSpeed;
+                amountMoved += moveSpeed;
             }
             else
             {
-
+                position.X -= moveSpeed;
+                amountMoved -= moveSpeed;
+            }
+            if (amountMoved == 0 || 
+                amountMoved == patrolLength)
+            {
+                patrolDirection = !patrolDirection;
             }
         }
     }
