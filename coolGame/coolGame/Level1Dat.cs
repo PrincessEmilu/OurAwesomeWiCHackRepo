0e099d5fcbb5d04477baf3f2e29b66b06de19bd4
Anna Murphy <blm1003@rit.edu>
2019-03-02 21:02:59 -0500

    added param to method

--- coolGame/coolGame/Level1Dat.cs
+++ coolGame/coolGame/Level1Dat.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace coolGame
 {
     class Level1Dat
     {
-        public List<Entity> GetEntities()
+        public List<Entity> GetEntities(Game game)
         {
             List<Entity> entities = new List<Entity>();
             entities.Add(new GuardEnemy(
-                null,
+                Content.Load<Texture2D>("enemy"),
                 new Microsoft.Xna.Framework.Rectangle(10, 10, 100, 100),
                 null));
             return entities;
