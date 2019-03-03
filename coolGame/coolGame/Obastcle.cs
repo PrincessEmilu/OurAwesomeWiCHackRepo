6a77c20ee54840cf668ec73d226a364587c798c7
Anna Murphy <blm1003@rit.edu>
2019-03-02 22:25:40 -0500

    class outlined

--- coolGame/coolGame/Obastcle.cs
+++ coolGame/coolGame/Obastcle.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace coolGame
 {
-    class Class1
+    class Obstacle : Entity
     {
+        public Obstacle(Texture2D texture, Rectangle position)
+            : base(texture, position) { }
+
+        public override bool CanBeHacked()
+        {
+            return false;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+        public override void Update(GameTime gameTime){}
     }
 }
