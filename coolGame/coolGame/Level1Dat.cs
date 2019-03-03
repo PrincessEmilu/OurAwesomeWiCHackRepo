699861d9e26dd568b9dee8fd38e221a586c61cdb
Anna Murphy <blm1003@rit.edu>
2019-03-02 20:16:53 -0500

    data file for level 1

---
+++ coolGame/coolGame/Level1Dat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coolGame
+{
+    static class Level1Dat
+    {
+        static public List<Entity> GetEntities()
+        {
+            List<Entity> entities = new List<Entity>();
+            return entities;
+        }
+
+        static public string GetBackground()
+        {
+            return null;
+        }
+    }
+}
