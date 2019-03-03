03c01997616cf7e99dcb535c87e724202dbee1c1
Anna Murphy <blm1003@rit.edu>
2019-03-02 23:01:55 -0500

    New Patrolling guard with small movement

---
+++ coolGame/coolGame/PatrolingGuard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace coolGame
+{
+    class PatrolingGuard : HackableEnemy
+    {
+
+        // Movement
+        const int moveSpeed = 5;
+        const int patrolLength = 200;
+        bool patrolDirection = true;
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (patrolDirection)
+            {
+                position.X += moveSpeed;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
