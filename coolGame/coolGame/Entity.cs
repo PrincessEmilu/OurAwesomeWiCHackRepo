928b46d7988ba851bfabb5959e9a8b625fbb1a70
Anna Murphy <blm1003@rit.edu>
2019-03-03 00:23:45 -0500

    added universalized move variables and a new method

--- coolGame/coolGame/Entity.cs
+++ coolGame/coolGame/Entity.cs
@@ -19,12 +19,57 @@ namespace coolGame
         protected Texture2D texture;
         protected Rectangle position;
 
+        // Movement information
+        protected int moveSpeed;
+        protected int patrolLength;
+        protected int amountMoved;
+        protected enum PatrolDirection
+        {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+        protected PatrolDirection currentDirection;
+
         public Entity (Texture2D texture, Rectangle position)
         {
             this.texture = texture;
             this.position = position;
+            this.moveSpeed = 0;
         }
 
         abstract public bool CanBeHacked();
+
+        public Rectangle GetFuturePosition ()
+        {
+            switch (currentDirection)
+            {
+                case (PatrolDirection.UP):
+                    return new Rectangle(this.position.X, 
+                        this.position.Y + this.moveSpeed,
+                        this.texture.Width, 
+                        this.texture.Height);
+                case (PatrolDirection.DOWN):
+                    return new Rectangle(this.position.X,
+                        this.position.Y - this.moveSpeed,
+                        this.texture.Width,
+                        this.texture.Height);
+                case (PatrolDirection.LEFT):
+                    return new Rectangle(this.position.X + this.moveSpeed,
+                        this.position.Y,
+                        this.texture.Width,
+                        this.texture.Height);
+                case (PatrolDirection.RIGHT):
+                    return new Rectangle(this.position.X - this.moveSpeed,
+                        this.position.Y,
+                        this.texture.Width,
+                        this.texture.Height);
+            }
+            return new Rectangle(this.position.X,
+                this.position.Y,
+                this.texture.Width,
+                this.texture.Height);
+        }
     }
 }
